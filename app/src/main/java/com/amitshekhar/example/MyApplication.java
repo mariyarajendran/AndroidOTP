2a249dba18f80bb26ca344120ec260e27c4bfd17
mariyarajendran <cgs-android@constient.com>
2019-09-19 18:56:21 +0530

    code shaping

--- app/src/main/java/com/amitshekhar/example/MyApplication.java
+++ app/src/main/java/com/amitshekhar/example/MyApplication.java
@@ -16,7 +16,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         mApplicationComponent = DaggerApplicationComponent.builder()
                 .applicationModule(new ApplicationModule(this))
                 .build();
--- app/src/main/java/com/amitshekhar/example/ui/screen/dashboardscreen/Dashboard.java
+++ app/src/main/java/com/amitshekhar/example/ui/screen/dashboardscreen/Dashboard.java
@@ -2,11 +2,8 @@ package com.amitshekhar.example.ui.screen.dashboardscreen;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
@@ -23,67 +20,59 @@ import com.amitshekhar.example.ui.screen.homescreen.FragmentHomeScreen;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class Dashboard extends BaseActivity {
 
-    Toolbar toolbar;
+    @BindView(R.id.dashboard_toolbar)
+    Toolbar mToolbar;
     ArrayList<String> navigation_items;
     private DashboardDrawerListAdapter dashboardDrawerListAdapter;
-    public ListView lv_drawer;
-    ViewPager viewPager;
-    BottomNavigationView navigation;
+    @BindView(R.id.dashboard_lv_drawer)
+    public ListView mListView;
+    @BindView(R.id.dashboard_drawer_layout)
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
         init();
-        SetDrawer();
-        toolbar.setTitle("Shop");
-        loadFragment(new FragmentHomeScreen());
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-
     }
 
     private void init() {
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+        addDrawerArrayData();
+        setDrawerLayout();
+        mToolbar.setTitle("Shop");
+        loadFragment(new FragmentHomeScreen());
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.content_dashboard_bottomnavigation_view);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
 
-        toolbar = (Toolbar) findViewById(R.id.toolbars);
-        setSupportActionBar(toolbar);
 
-
+    public void addDrawerArrayData() {
         navigation_items = new ArrayList<>();
-
-//adding menu items for naviations
         navigation_items.add("Call");
         navigation_items.add("Favorite");
         navigation_items.add("Search");
-
-        lv_drawer = (ListView) findViewById(R.id.lv_drawer);
-
     }
 
-    private void SetDrawer() {
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    private void setDrawerLayout() {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, drawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
         toggle.syncState();
-
         dashboardDrawerListAdapter = new DashboardDrawerListAdapter(Dashboard.this, navigation_items);
-        lv_drawer.setAdapter(dashboardDrawerListAdapter);
-
-        lv_drawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mListView.setAdapter(dashboardDrawerListAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-
             }
         });
-
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -94,22 +83,22 @@ public class Dashboard extends BaseActivity {
             Fragment fragment;
             switch (item.getItemId()) {
                 case R.id.bottom_navigation_item_home:
-                    toolbar.setTitle("Shop");
+                    mToolbar.setTitle("Shop");
                     fragment = new FragmentHomeScreen();
                     loadFragment(fragment);
                     return true;
                 case R.id.bottom_navigation_item_email:
-                    toolbar.setTitle("My Gifts");
+                    mToolbar.setTitle("My Gifts");
                     fragment = new FragmentAccountScreen();
                     loadFragment(fragment);
                     return true;
                 case R.id.bottom_navigation_item_search:
-                    toolbar.setTitle("Cart");
+                    mToolbar.setTitle("Cart");
                     fragment = new FragmentHomeScreen();
                     loadFragment(fragment);
                     return true;
                 case R.id.bottom_navigation_item_account:
-                    toolbar.setTitle("Profile");
+                    mToolbar.setTitle("Profile");
                     fragment = new FragmentAccountScreen();
                     loadFragment(fragment);
                     return true;
@@ -121,7 +110,7 @@ public class Dashboard extends BaseActivity {
 
     private void loadFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_container, fragment);
+        transaction.replace(R.id.content_dashboard_framelayout, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
